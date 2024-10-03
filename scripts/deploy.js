7f39782b07279af444f786086f5fd762a10077f4
Ahmet Yortanli <ahmetyortanli@gmail.com>
2024-10-03 11:00:51 +0300

    Hardhat deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,61 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+  const [buyer, seller, inspector, lender] = await ethers.getSigners();
 
+  const RealEstate = await ethers.getContractFactory("RealEstate");
+  const realEstate = await RealEstate.deploy();
+  await realEstate.deployed();
+
+  console.log("Deployed Real Estate Contract at: " + realEstate.address);
+  console.log("Minting 3 properties...\n");
+  for (let i = 1; i <= 3; ++i) {
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        `https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i}.json`
+      );
+    await transaction.wait();
+  }
+
+  const Escrow = await ethers.getContractFactory("Escrow");
+  const escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  );
+
+  console.log("Approving Properties...");
+  for (let i = 1; i <= 3; ++i) {
+    let transaction = await realEstate
+      .connect(seller)
+      .approve(escrow.address, i);
+    await transaction.wait();
+  }
+
+  console.log("Listing Properties...");
+  let transaction = await escrow
+    .connect(seller)
+    .list(1, buyer.address, tokens(20), tokens(10));
+  transaction.wait();
+  transaction = await escrow
+    .connect(seller)
+    .list(2, buyer.address, tokens(15), tokens(5));
+  transaction.wait();
+  transaction = await escrow
+    .connect(seller)
+    .list(3, buyer.address, tokens(10), tokens(5));
+  transaction.wait();
+
+  console.log("Finished.");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
